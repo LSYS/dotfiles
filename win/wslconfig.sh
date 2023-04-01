b076eef24ae9fe06c09afd169c2e99533b29d024
LSYS <lucas@lucasshen.com>
2023-04-01 17:24:25 +0800

    Add wslconfig

---
+++ win/wslconfig.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# defaults
+DOTFILES_MASTER="$HOME/dotfiles"
+INSTALL_PATH="$HOME"
+
+echo "==================" 
+echo "Install wslconfig"
+echo "=================" 
+echo "Install .wslconfig to the system."
+echo
+echo "Usage: $0"
+echo "       -p installation path (default: $HOME)"
+echo
+echo "Example: install [<path_to_install_to>]"
+echo
+echo "--------------------------------------------" 
+
+install() {
+    local username=$(whoami.exe)
+    username=$(echo $username | awk -F '\\' '{print $2}' | tr -d '\r')
+    local installpath="/mnt/c/Users/$username/"
+    echo "Username: $username"
+
+    SRC="$DOTFILES_MASTER/win/.wslconfig"
+    if [ -e "$SRC-$HOSTNAME" ]; then
+        SRC="$SRC-$HOSTNAME"
+    fi
+    echo "Source file: $SRC"
+
+    if [ -f "$SRC" ]; then
+        echo "Installing $SRC to $installpath:"
+        ln -sfv "$SRC" "$installpath/.wslconfig"
+    else
+        echo "Error: $SRC does not exist"
+        return 1
+    fi
+}
+
+install
